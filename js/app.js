19821cfbb09d7c6085cc5449edebb26ee2ed7ed1
Gianluca Vasaturo <g.vasaturo@gmail.com>
2017-05-10 19:12:43 +0200

    signup gialla

--- js/app.js
+++ js/app.js
@@ -1,16 +1,1 @@
- angular.module('andlollo',[])
-     .directive('wjValidationError', function () {
-    return {
-        require: 'ngModel',
-        link: function (scope, elm, attrs, ctl) {
-            scope.$watch(attrs['wjValidationError'], function (errorMsg) {
-                elm[0].setCustomValidity(errorMsg);
-                ctl.$setValidity('wjValidationError', errorMsg ? false : true);
-                console.log('elm'+elm);
-                console.log('scope'+scope);
-                console.log('attrs'+attrs);
-                console.log('ctl'+ctl);
-            });
-        }
-    };
-});
+ angular.module('andlollo',[]);
